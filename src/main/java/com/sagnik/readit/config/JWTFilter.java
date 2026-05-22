686910cb4bac272964ffa484aeeaabc4168b2c1b
sagnikghosh <sagnikghosh904@gmail.com>
2026-05-22 11:22:40 +0530

    Send back jwt token instead of storing into cookie

--- src/main/java/com/sagnik/readit/config/JWTFilter.java
+++ src/main/java/com/sagnik/readit/config/JWTFilter.java
@@ -3,7 +3,6 @@ package com.sagnik.readit.config;
 import com.sagnik.readit.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 @Component
 public class JWTFilter extends OncePerRequestFilter {
@@ -31,18 +29,14 @@ public class JWTFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
 
-
-        final String jwt = Arrays.stream(request.getCookies() != null ? request.getCookies() : new Cookie[0])
-                .filter(c -> "jwt".equals(c.getName()))
-                .map(Cookie::getValue)
-                .findFirst()
-                .orElse(null);
-
-        if (jwt == null) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
+
+        String jwt = authHeader.substring(7);
 
         try {
             final String userEmail = jwtService.extractUsername(jwt);
--- src/main/java/com/sagnik/readit/controller/AuthController.java
+++ src/main/java/com/sagnik/readit/controller/AuthController.java
@@ -4,20 +4,15 @@ import com.sagnik.readit.requestDto.UserRequestDto;
 import com.sagnik.readit.responseDto.UserResponseDto;
 import com.sagnik.readit.service.JwtService;
 import com.sagnik.readit.service.UserService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -39,28 +34,7 @@ public class AuthController {
         logger.info("Logging in user with request {}", user);
         UserResponseDto userResponseDto = userService.login(user);
         String token = jwtService.generateToken(userResponseDto.username());
-        ResponseCookie cookie = ResponseCookie.from("jwt", token)
-                .httpOnly(true)
-                .secure(true)
-                .sameSite("Strict")
-                .path("/")
-                .maxAge(Duration.ofMillis(EXPIRATION_TIME))
-                .build();
-
         logger.info("Successfully logged in user with Id {}", userResponseDto.id());
-        return ResponseEntity.status(201)
-                .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .build();
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<String> post(HttpServletResponse response) {
-        Cookie cookie = new Cookie("jwt", null);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-        return ResponseEntity.ok("Cookie Deleted Successfully");
+        return new ResponseEntity<>(token, HttpStatus.CREATED);
     }
 }
